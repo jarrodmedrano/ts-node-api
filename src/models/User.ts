6162cd35214c0da1a2567a7fcf2969ea3779cbc1
Jarrod Medrano <jmedran@gmail.com>
2019-02-27 19:58:12 -0700

    setting up post router class

---
+++ src/models/User.ts
@@ -0,0 +1,35 @@
+import {model, Schema} from "mongoose";
+
+let UserSchema: Schema = new Schema({
+    createdAt: Date,
+    updatedAt: Date,
+    name: {
+        type: String,
+        default: '',
+        required: true
+    },
+    username: {
+        type: String,
+        default: '',
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        default: '',
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        default: '',
+        required: true
+    },
+    featuredImage: {
+        type: String,
+        default: ''
+    }
+});
+
+export default model('Post', UserSchema);
---
+++ src/router/PostRouter.ts
@@ -0,0 +1,6 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import Post from '../models/Post';
+
+class PostRouter {
+
+}
