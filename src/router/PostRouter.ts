c4c3b83341d21cf4221c224c7149dc93ec8c888e
Jarrod Medrano <jmedran@gmail.com>
2019-03-04 17:15:27 -0600

    creating routes

--- src/router/PostRouter.ts
+++ src/router/PostRouter.ts
@@ -54,19 +54,78 @@ class PostRouter {
     }
 
     public CreatePost(req: Request, res: Response): void {
+        const title: string = req.body.title;
+        const slug: string = req.body.slug;
+        const content: string = req.body.content;
+        const featuredImage: string = req.body.featuredImage;
 
+
+        const post = new Post({
+            title,
+            slug,
+            content,
+            featuredImage
+        });
+
+        post.save()
+            .then((data) => {
+            const status = res.statusCode;
+            res.json({
+                status,
+                data
+            })
+        })
     }
 
     public UpdatePost(req: Request, res: Response): void {
+        const slug: string = req.params.slug;
 
+        Post.findOneAndUpdate({ slug }, req.body).then((data) => {
+
+            const status = res.statusCode;
+
+            res.json({
+                status,
+                data
+            });
+
+        }).catch((err) => {
+
+            const status = res.statusCode;
+            res.json({
+                status,
+                err
+            })
+        })
     }
 
     public DeletePost(req: Request, res: Response): void {
+        const slug: string = req.params.slug;
 
+        Post.findOneAndRemove({ slug }).then((data) => {
+
+            const status = res.statusCode;
+
+            res.json({
+                status,
+                data
+            });
+
+        }).catch((err) => {
+
+            const status = res.statusCode;
+            res.json({
+                status,
+                err
+            })
+        })
     }
 
     routes() {
         this.router.get('/', this.GetPosts);
+        this.router.get('/:slug', this.GetPost);
+        this.router.get('/', this.CreatePost);
+        this.router.get('/:slug', this.DeletePost);
     }
 }
 
